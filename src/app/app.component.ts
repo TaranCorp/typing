fe05e018e522d7b2270f64808276160000c01972
jakub.taran <jakub.taran@ttms.pl>
2023-01-03 12:32:25 +0100

    *Implement typing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  title = 'Typing Challenge';
+  typingExample = lorem.sentence();
+  userAnswer = '';
+  spliterator = "";
+
+  matchAnswer(target: any) {
+    this.userAnswer = target.value;
+  }
+
+  checkAnswer() {
+    return this.typingExample === this.userAnswer;
+  }
+
+  compare(exampleLetter: string, userAnswerLetter: string | undefined) {
+    if (!userAnswerLetter) {
+      return "empty-answer";
+    }
+    return exampleLetter === userAnswerLetter 
+            ? 'match' 
+            : 'not-match';
+  }
+
+  getMatchingPercentage() {
+    const splittedExample = this.typingExample.split('');
+    let letterMatchCounter = 0;
+
+    for (let i = 0; i <= this.userAnswer.length; i++) {
+      if (this.areValuesDefined(splittedExample, i) 
+          && splittedExample[i] === this.userAnswer[i]) {
+        letterMatchCounter++;
+      }
+    }
+    
+    return Math.round(
+      this.calculatePercentage(letterMatchCounter, splittedExample)
+    );
+  }
+
+  private areValuesDefined(splittedExample: string[], i: number) {
+    return splittedExample[i] &&
+      this.userAnswer[i];
+  }
+
+  private calculatePercentage(letterMatchCounter: number, splittedExample: string[]): number {
+    return letterMatchCounter * 100 / splittedExample.length;
+  }
 }
